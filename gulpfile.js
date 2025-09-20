43864d89d11469cd95fac02078fe3d69df9d212d
ianca <iancalaurentino@gmai.com>
2025-09-19 23:08:02 -0300

    Adiciona hero, funcionalidades e seção da informações com animação CSS.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function styles() {
+    return gulp.src('./src/styles/*.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/styles'));
+}
+
+
+exports.default = gulp.parallel(styles);
+
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+};
